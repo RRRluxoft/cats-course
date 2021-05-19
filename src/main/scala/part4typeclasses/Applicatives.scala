f170af576423f5895f18725539830520e6fda7a5
Roman Lytvyshko <roman.lytvyshko@ocado.com>
2021-05-19 17:48:49 +0200

    Applicatives

--- src/main/scala/part4typeclasses/Applicatives.scala
+++ src/main/scala/part4typeclasses/Applicatives.scala
@@ -6,10 +6,10 @@ object Applicatives {
   import cats.Applicative
   import cats.instances.list._
   val listApplicative = Applicative[List]
-  val aList = listApplicative.pure(2) // List(2)
+  val aList: List[Int] = listApplicative.pure(2) // List(2)
 
   import cats.instances.option._ // implicit Applicative[Option]
-  val optionApplicative = Applicative[Option]
+  val optionApplicative: Applicative[Option] = Applicative[Option]
   val anOption = optionApplicative.pure(2) // Some(2)
 
   // pure extension method
@@ -28,7 +28,8 @@ object Applicatives {
 
   // TODO: thought experiment
   // def ap[W[_], B, T](wf: W[B => T])(wa: W[B]): W[T] = ??? // this already implemented
-  def productWithApplicatives[W[_], A, B](wa: W[A], wb: W[B])(implicit applicative: Applicative[W]): W[(A, B)] = {
+  import cats.syntax.functor._
+  def productWithApplicatives[W[_] /* : Applicative */, A, B](wa: W[A], wb: W[B])(implicit applicative: Applicative[W]): W[(A, B)] = {
     val functionWrapper: W[B => (A, B)] = applicative.map(wa)(a => (b: B) => (a, b))
     applicative.ap(functionWrapper)(wb)
   }
