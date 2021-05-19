e8eb50dd2afcbd6118ff92e0f4d8b01a5334d850
Roman Lytvyshko <roman.lytvyshko@ocado.com>
2021-05-19 23:23:41 +0200

    HandlingErrors

--- src/main/scala/part4typeclasses/HandlingErrors.scala
+++ src/main/scala/part4typeclasses/HandlingErrors.scala
@@ -24,25 +24,29 @@ object HandlingErrors {
   import cats.instances.either._ // implicit MonadError
   type ErrorOr[A] = Either[String, A]
   val monadErrorEither = MonadError[ErrorOr, String]
-  val success = monadErrorEither.pure(32) // Either[String, Int] == Right(32)
-  val failure = monadErrorEither.raiseError[Int]("something wrong") // Either[String, Int] == Left("something wrong")
+  val success: ErrorOr[Int] = monadErrorEither.pure(32) // Either[String, Int] == Right(32)
+  val failure: ErrorOr[Int] = monadErrorEither.raiseError[Int]("something wrong") // Either[String, Int] == Left("something wrong")
+
   // "recover"
   val handledError: ErrorOr[Int] = monadErrorEither.handleError(failure) {
     case "Badness" => 44
     case _ => 89
   }
+
   // "recoverWith"
   val handledError2: ErrorOr[Int] = monadErrorEither.handleErrorWith(failure) {
     case "Badness" => monadErrorEither.pure(44) // ErrorOr[Int]
     case _ => Left("Something else") // ErrorOr[Int]
   }
+
   // "filter"
-  val filteredSuccess = monadErrorEither.ensure(success)("Number too small")(_ > 100)
+  val filteredSuccess: ErrorOr[Int] = monadErrorEither.ensure(success)("Number too small")(_ > 100)
 
   // Try and Future
   import cats.instances.try_._ // implicit MonadError[Try], E = Throwable
   val exception = new RuntimeException("Really bad")
   val pureException: Try[Int] = MonadError[Try, Throwable].raiseError(exception) // Failure(exception)
+
   import cats.instances.future._
   implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
   MonadError[Future, Throwable].raiseError(exception) // Future which will complete with a Failure(exception)
